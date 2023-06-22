c6e7f46f50fb540b309b57cf42123d050e1ec8ce
Dominika Buczek-Szkowron <buczekszkowron@gmail.com>
2023-06-22 11:27:10 +0100

    style component Container and create theme

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import Form from "./Form/form.js";
+import { Container } from "./styled";
+import { ThemeProvider } from "styled-components";
+
+const theme = {
+  colors: {
+    primaryColor: "black",
+    secondaryColor: "hsl(240, 67%, 75%)",
+  },
+};
 
 function App() {
   return (
-    <div className="container">
-      <Form
-      // setAmount={setAmount}
-      />
-    </div>
+    <ThemeProvider theme={theme}>
+      <Container>
+        <Form />
+      </Container>
+    </ThemeProvider>
   );
 }
 
